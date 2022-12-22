a1c6ebe3614db90cd887170a608f96b0eb03f862
kunhonrh321 <habeebrh321@gmail.com>
2022-12-23 02:51:48 +0530

    starting screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:scotch/home_screen.dart';
+import 'package:get/get.dart';
+import 'package:scotch/view/startingscreen/view/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: false,
+    tools: const [
+      ...DevicePreview.defaultTools,
+    ],
+    builder: (BuildContext context) {
+      return const MyApp();
+    },
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +20,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Scotch',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const StartingScreen(),
     );
   }
 }
---
+++ lib/view/startingscreen/view/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:scotch/core/const.dart';
+
+class StartingScreen extends StatelessWidget {
+  const StartingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        children: [
+          const LogingStackImage(),
+          ScotchGetStarted(size: size),
+        ],
+      ),
+    );
+  }
+}
+
+class ScotchGetStarted extends StatelessWidget {
+  const ScotchGetStarted({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Text(
+            'Scotch',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              'An E-commerce application used to buying and selling of goods and services, or the transmitting of funds or data.',
+              style: TextStyle(fontSize: 17),
+            ),
+          ),
+          Container(
+            height: size.height * 0.07,
+            width: size.width * 0.5,
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Text(
+                    'Get started for free',
+                    style: TextStyle(color: kWhitecolor),
+                  ),
+                  Icon(
+                    Icons.keyboard_arrow_right,
+                    color: kWhitecolor,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LogingStackImage extends StatelessWidget {
+  const LogingStackImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final stackSize = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Container(
+          height: stackSize.height * 0.5,
+          width: stackSize.width,
+          decoration: const BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(140),
+              bottomRight: Radius.circular(140),
+            ),
+          ),
+        ),
+        Positioned(
+          top: stackSize.height * 0.3,
+          left: stackSize.width * 0.33,
+          child: const CircleAvatar(
+            radius: 60,
+            backgroundColor: kBlackcolor,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundColor: kWhitecolor,
+              backgroundImage: AssetImage('assets/logo/scotch.png'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
