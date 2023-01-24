b4f620059809c256132aceba226beb90a750ba63
kunhonrh321 <habeebrh321@gmail.com>
2023-01-24 22:22:48 +0530

    cart updated

--- lib/view/screens/product_screen/view/widgets/add_to_cart_des_button.dart
+++ lib/view/screens/product_screen/view/widgets/add_to_cart_des_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:scotch/core/const.dart';
 import 'package:scotch/view/screens/cart_screen/controller/cart_controller.dart';
-import 'package:scotch/view/screens/cart_screen/view/cart_screen.dart';
 import 'package:scotch/view/screens/home_screen/model/product_model.dart';
 
 class AddToCartDesButton extends StatelessWidget {
@@ -16,54 +15,103 @@ class AddToCartDesButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     CartController cartController = Get.put(CartController());
-    return GetBuilder<CartController>(builder: (context) {
-      return Expanded(
-        child: InkWell(
-          onTap: () {
-            cartController.addToCart(homeCtr.id, homeCtr.size.toString());
-            Get.to(const CartScreen());
-          },
-          child: Container(
-            height: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              gradient: LinearGradient(
-                begin: const FractionalOffset(0, 0),
-                end: const FractionalOffset(0.8, 0),
-                stops: const [
-                  0,
-                  0.6,
-                ],
-                tileMode: TileMode.clamp,
-                colors: [
-                  themeColor,
-                  Colors.blue.shade500,
-                ],
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  Icons.shopping_cart_outlined,
-                  color: kWhitecolor,
-                  size: 25,
-                ),
-                Text(
-                  'Add To Cart',
-                  style: TextStyle(
-                    color: kWhitecolor,
-                    fontFamily: "Montserrat",
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1,
-                    fontSize: 18,
+    return GetBuilder<CartController>(
+      builder: (context) {
+        return cartController.cartItemsId.contains(homeCtr.id)
+            ? Expanded(
+                child: InkWell(
+                  onTap: () {
+                    cartController.gotToCartFromProduct();
+                  },
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      gradient: LinearGradient(
+                        begin: const FractionalOffset(0, 0),
+                        end: const FractionalOffset(0.8, 0),
+                        stops: const [
+                          0,
+                          0.6,
+                        ],
+                        tileMode: TileMode.clamp,
+                        colors: [
+                          themeColor,
+                          Colors.blue.shade500,
+                        ],
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Icon(
+                          Icons.shopping_cart_outlined,
+                          color: kWhitecolor,
+                          size: 25,
+                        ),
+                        Text(
+                          'Go To Cart',
+                          style: TextStyle(
+                            color: kWhitecolor,
+                            fontFamily: "Montserrat",
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-      );
-    });
+              )
+            : Expanded(
+                child: InkWell(
+                  onTap: () {
+                    cartController.addToCart(
+                        homeCtr.id, homeCtr.size.toString());
+                  },
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      gradient: LinearGradient(
+                        begin: const FractionalOffset(0, 0),
+                        end: const FractionalOffset(0.8, 0),
+                        stops: const [
+                          0,
+                          0.6,
+                        ],
+                        tileMode: TileMode.clamp,
+                        colors: [
+                          themeColor,
+                          Colors.blue.shade500,
+                        ],
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Icon(
+                          Icons.shopping_cart_outlined,
+                          color: kWhitecolor,
+                          size: 25,
+                        ),
+                        Text(
+                          'Add To Cart',
+                          style: TextStyle(
+                            color: kWhitecolor,
+                            fontFamily: "Montserrat",
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+      },
+    );
   }
 }
