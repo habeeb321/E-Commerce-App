344d32027bae3d77f4b3fc46ca0c89aa0d6adf4e
kunhonrh321 <habeebrh321@gmail.com>
2023-01-30 07:24:56 +0530

    badge implemented in cart

--- lib/view/screens/bottom_nav_bar/view/bottom_nav_bar.dart
+++ lib/view/screens/bottom_nav_bar/view/bottom_nav_bar.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:scotch/core/const.dart';
+import 'package:scotch/view/screens/cart_and_order_address_payment_controller/cart_and_order_address_payment_controller.dart';
 import 'package:scotch/view/screens/profile_screen/view/profile_screen.dart';
 import 'package:scotch/view/screens/bottom_nav_bar/controller/bottom_nav_controller.dart';
 import 'package:scotch/view/screens/cart_screen/view/cart_screen.dart';
 import 'package:scotch/view/screens/home_screen/view/home_screen.dart';
 import 'package:scotch/view/screens/wishlist_screen/view/wishlist_screen.dart';
+import 'package:badges/badges.dart' as badges;
 
 class BottomNavBar extends GetView<BottomNavController> {
   const BottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    CoaController coaController = Get.put(CoaController());
+    BottomNavController bottomNavController = Get.put(BottomNavController());
     int currentIndex = 0;
 
     List pages = [
@@ -41,21 +45,41 @@ class BottomNavBar extends GetView<BottomNavController> {
                 vertical: Get.size.height * 0.015,
               ),
               child: GNav(
-                tabs: const [
-                  GButton(
+                tabs: [
+                  const GButton(
                     icon: Icons.home_outlined,
                     iconSize: 30,
                     text: 'Home',
                   ),
                   GButton(
                       icon: Icons.shopping_cart_outlined,
+                      leading: GetBuilder<CoaController>(
+                        builder: (controller) {
+                          return badges.Badge(
+                            position:
+                                badges.BadgePosition.topEnd(top: -17, end: -13),
+                            badgeContent: Text(
+                              coaController.totalproductCount.toString(),
+                              style: const TextStyle(
+                                color: kWhitecolor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            child: Icon(Icons.shopping_cart_outlined,
+                                color: bottomNavController.currentIndex == 1
+                                    ? lightThemeColor
+                                    : kWhitecolor,
+                                size: 30),
+                          );
+                        },
+                      ),
                       iconSize: 30,
                       text: 'Cart'),
-                  GButton(
+                  const GButton(
                       icon: Icons.favorite_border_outlined,
                       iconSize: 30,
                       text: 'Wishlist'),
-                  GButton(
+                  const GButton(
                       icon: Icons.person_outline,
                       iconSize: 30,
                       text: 'Account'),
