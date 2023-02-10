dcd5b4b71a283d8fd1f0d505b66d42e8b3859d4b
kunhonrh321 <habeebrh321@gmail.com>
2023-02-10 14:53:41 +0530

    order screen updated

--- lib/view/screens/order_details/view/order_details.dart
+++ lib/view/screens/order_details/view/order_details.dart
@@ -1,29 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:scotch/view/screens/widgets/navigator_key.dart';
+// import 'package:flutter/material.dart';
+// import 'package:scotch/view/screens/widgets/navigator_key.dart';
 
-class OrderDetials extends StatelessWidget {
-  const OrderDetials({super.key, required this.orderId});
+// class OrderDetials extends StatelessWidget {
+//   const OrderDetials({super.key, required this.orderId});
 
-  final String orderId;
+//   final String orderId;
 
-  @override
-  Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      NavigationService.navigatorKey.currentContext;
-    });
-    return const Scaffold(
-      body: Center(child: Text('order')),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+//       NavigationService.navigatorKey.currentContext;
+//     });
+//     return const Scaffold(
+//       body: Center(child: Text('order')),
+//     );
+//   }
+// }
 
-class CancelOrder extends StatelessWidget {
-  const CancelOrder({super.key});
+// class CancelOrder extends StatelessWidget {
+//   const CancelOrder({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('cancel')),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: Center(child: Text('cancel')),
+//     );
+//   }
+// }
